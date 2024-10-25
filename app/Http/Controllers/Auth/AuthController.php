c8d877d15bdfd7bbbf4349655d3379f8731e5f0a
Shalala <shalala.shafiyeva23@gmail.com>
2024-10-26 01:06:10 +0400

    Login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,12 +5,39 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    public function login(Request $request) {}
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|max:255',
+            'password' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                "errors" => $validator->errors(),
+            ], 422);
+        }
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+            //$token = $user->createToken('unifurniture');
+            return response()->json([
+                "data" => $user,
+               // "token" => $token->plainTextToken,
+                "message" => "User logged in successfully",
+                "success" => true
+            ], 200);
+        } else {
+            return response()->json([
+                "message" => "Invalid email or password",
+                "success" => false
+            ], 401);
+        }
+    }
 
     public function register(Request $request)
     {
