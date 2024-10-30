9b5cf86e5b2e73e5258517f1e506a76d560a1af6
Shalala <shalala.shafiyeva23@gmail.com>
2024-10-30 04:49:11 +0400

    Continue methods for About page(AboutController)

--- routes/api.php
+++ routes/api.php
@@ -19,12 +19,55 @@ Route::middleware('auth:sanctum')->group(function () {
     //basket
 
     //DASHBOARD
-    Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.'], function () {
+    Route::group(['prefix' => 'dashboard'], function () {
         //ABOUT PAGE
-        Route::group(['prefix' => 'about', 'as' => 'about.'], function () {
-            Route::get('/banner', [AboutController::class, 'getBanners']);
-            Route::post('/banner', [AboutController::class, 'createBanner']);
-            Route::post('/paralax', [AboutController::class, 'createParalax']);
+        Route::group(['prefix' => 'about'], function () {
+            Route::group(['prefix' => 'banner'], function () {
+                Route::get('/', [AboutController::class, 'getBanners']);
+                Route::get('/{id}', [AboutController::class, 'getBanner']);
+                Route::post('/create', [AboutController::class, 'createBanner']);
+                Route::delete('/delete/{id}', [AboutController::class, 'deleteBanner']);
+                Route::post('/edit/{id}', [AboutController::class, 'editBanner']);
+                Route::post('/publish/{id}', [AboutController::class, 'publishBanner']);
+            });
+            Route::group(['prefix' => 'paralax'], function () {
+                Route::get('/', [AboutController::class, 'getParalaxes']);
+                Route::get('/{id}', [AboutController::class, 'getParalax']);
+                Route::post('/create', [AboutController::class, 'createParalax']);
+                Route::delete('/delete/{id}', [AboutController::class, 'deleteParalax']);
+                Route::post('/edit/{id}', [AboutController::class, 'editParalax']);
+                Route::post('/publish/{id}', [AboutController::class, 'publishParalax']);
+            });
+            Route::group(['prefix' => 'team-title'], function () {
+                Route::get('/', [AboutController::class, 'getTeamTitles']);
+                Route::get('/{id}', [AboutController::class, 'getTeamTitle']);
+                Route::post('/create', [AboutController::class, 'createTeamTitle']);
+                Route::delete('/delete/{id}', [AboutController::class, 'deleteTeamTitle']);
+                Route::post('/edit/{id}', [AboutController::class, 'editTeamTitle']);
+                Route::post('/publish/{id}', [AboutController::class, 'publishTitle']);
+            });
+            Route::group(['prefix' => 'team-member'], function () {
+                Route::get('/', [AboutController::class, 'getTeamMembers']);
+                Route::get('/{id}', [AboutController::class, 'getTeamMember']);
+                Route::post('/create', [AboutController::class, 'createTeamMember']);
+                Route::delete('/delete/{id}', [AboutController::class, 'deleteTeamMember']);
+                Route::post('/edit/{id}', [AboutController::class, 'editTeamMember']);
+            });
+            Route::group(['prefix' => 'number-title'], function () {
+                Route::get('/', [AboutController::class, 'getNumberTitles']);
+                Route::get('/{id}', [AboutController::class, 'getNumberTitle']);
+                Route::post('/create', [AboutController::class, 'createNumberTitle']);
+                Route::delete('/delete/{id}', [AboutController::class, 'deleteNumberTitle']);
+                Route::post('/edit/{id}', [AboutController::class, 'editNumberTitle']);
+                Route::post('/publish/{id}', [AboutController::class, 'publishNumberTitle']);
+            });
+            Route::group(['prefix' => 'number-subtitle'], function () {
+                Route::get('/', [AboutController::class, 'getNumberSubtitles']);
+                Route::get('/{id}', [AboutController::class, 'getNumberSubtitle']);
+                Route::post('/create', [AboutController::class, 'createNumberSubtitle']);
+                Route::delete('/delete/{id}', [AboutController::class, 'deleteNumberSubtitle']);
+                Route::post('/edit/{id}', [AboutController::class, 'editNumberSubtitle']);
+            });
         });
     });
 });
@@ -35,3 +78,11 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
 Route::post('/logout', [AuthController::class, 'logout']);
+
+//ABOUT PAGE
+Route::get('/about/banner', [AboutController::class, 'publishedBanner']);
+Route::get('/about/paralax', [AboutController::class, 'publishedParalax']);
+Route::get('/about/team-title', [AboutController::class, 'publishedTeamTitle']);
+Route::get('/about/team-member', [AboutController::class, 'getTeamMembers']);
+Route::get('/about/number-title', [AboutController::class, 'publishedNumberTitle']);
+Route::get('/about/number-subtitle', [AboutController::class, 'getNumberSubtitles']);
